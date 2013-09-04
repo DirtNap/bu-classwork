34b085f2fd1a5bd4f421d5d2844314943d1b5994
Michael Donnelly <MichaelVDonnelly@gmail.com>
2013-09-04 08:30:12 -0400

    Final State of Assignment 1

---
+++ CS232 - Assignment #1/src/FirstProgram.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class FirstProgram {
+	public static void main (String [] args) {
+		System.out.println("Hello out there Michael Donnelly.");
+		System.out.println("I will add two numbers for you.");
+		System.out.println("Enter two whole numbers on a line:");
+		int n1, n2;
+		Scanner keyboard = new Scanner(System.in);
+		n1 = keyboard.nextInt();
+		n2 = keyboard.nextInt();
+		System.out.println("The sum of those two numbers is");
+		System.out.println(n1 + n2);
+	}
+}
