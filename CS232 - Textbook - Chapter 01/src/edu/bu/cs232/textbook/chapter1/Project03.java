c4b712ba0172595254136fa482b080a27a69c3ff
Michael Donnelly <MichaelVDonnelly@gmail.com>
2013-09-07 15:59:51 -0400

    Chapter 1 complete.

--- CS232 - Textbook - Chapter 01/src/edu/bu/cs232/textbook/chapter1/Project03.java
+++ CS232 - Textbook - Chapter 01/src/edu/bu/cs232/textbook/chapter1/Project03.java
@@ -1,10 +1,21 @@
 package edu.bu.cs232.textbook.chapter1;
 
 public class Project03 {
-	public void printSquare(char useChar, int sideSize) {
-		System.out.println(useChar * sideSize);
-	}
 	public static void main (String [] args) {
-		System.out.println(String.format(":%5s:","a"));
+		System.out.println("     **************\n");
+		System.out.println("    *            **\n");
+		System.out.println("   *            * *\n");
+		System.out.println("  *            *  *\n");
+		System.out.println(" *            *   *\n");
+		System.out.println("**************    *\n");
+		System.out.println("*            *    *\n");
+		System.out.println("*            *    *\n");
+		System.out.println("*            *    *\n");
+		System.out.println("*            *    *\n");
+		System.out.println("*            *   *\n");
+		System.out.println("*            *  *\n");
+		System.out.println("*            * *\n");
+		System.out.println("*            **\n");
+		System.out.println("**************");
 	}
 }
