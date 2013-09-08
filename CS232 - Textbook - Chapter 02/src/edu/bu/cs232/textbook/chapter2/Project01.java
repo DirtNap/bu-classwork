04bd42ed5787462e98e235ac70bdc7052c8b4bb8
Michael Donnelly <MichaelVDonnelly@gmail.com>
2013-09-08 09:10:06 -0400

    Initial import of project.

---
+++ CS232 - Textbook - Chapter 02/src/edu/bu/cs232/textbook/chapter2/Project01.java
@@ -0,0 +1,13 @@
+package edu.bu.cs232.textbook.chapter2;
+
+public class Project01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
