5e83bfaef1b1f58f4f781b1f479cd1763d21af8a
Michael Donnelly <MichaelVDonnelly@gmail.com>
2013-12-02 13:52:31 -0500

    Finalize Assignment 04

--- CS232 - Assignment #4/src/edu/bu/cs232/Assignment04.java
+++ CS232 - Assignment #4/src/edu/bu/cs232/Assignment04.java
@@ -6,6 +6,11 @@ public class Assignment04 extends Assignment03 {
 		super(reader);
 	}
 
+	@Override
+	protected Shopper createShopper(int itemCount, double budget) {
+		SortingShopper ss = new SortingShopper(itemCount, budget);
+		return (Shopper) ss;
+	}
 
 	public static void main(String[] args) {
 		Assignment04 self = new Assignment04(new InputReader(System.in, System.out));
--- CS232 - Assignment #4/src/edu/bu/cs232/SortingShopper.java
+++ CS232 - Assignment #4/src/edu/bu/cs232/SortingShopper.java
@@ -1,10 +1,9 @@
 package edu.bu.cs232;
 
 public class SortingShopper extends Shopper {
-	//private SortingShoppingList shoppingList;
 	public SortingShopper(int itemCount, double budget) {
 		super(itemCount, budget);
-		// TODO Auto-generated constructor stub
+		this.getShoppingList().setSortProvider(new ListSorterSortProvider());
 	}
 
 }
