15af7e7761c9321b85acded736a0d8e06591c522
Michael Donnelly <MichaelVDonnelly@gmail.com>
2014-01-28 16:11:23 -0500

    Added documentation.

--- CS342_-_Homework_01/src/edu/bu/cs342/hw01/NumberGuessingGame.java
+++ CS342_-_Homework_01/src/edu/bu/cs342/hw01/NumberGuessingGame.java
@@ -12,39 +12,98 @@ public class NumberGuessingGame {
     private final PrintStream out;
     private final PrintStream err;
     private int solution;
+    private static Random prng;
 
+    /**
+     * A number guessing game. The user selects a number between {@code 1} and
+     * some upper boundary. The class provides feedback on the guess, indicating
+     * if it was too high, too low, or correct. The number of moves required to
+     * reach the correct answer is provided to the user at the end of the game.
+     */
     public NumberGuessingGame() {
         this(1000);
     }
 
+    /**
+     * Allows the upper bound to be selected.
+     * 
+     * @param maxValue
+     *            the upper bound with which the game should be played.
+     */
     public NumberGuessingGame(int maxValue) {
         this(maxValue, new Scanner(System.in), System.out, System.err);
     }
 
+    /**
+     * Allows the input/output options to be configured.
+     * 
+     * @param scanner
+     *            a configured java.util.Scanner from which to read input
+     * @param out
+     *            an open java.io.PrintStream to provide feedback
+     * @param err
+     *            an open java.io.PrintStream to display errors
+     */
     public NumberGuessingGame(int maxValue, Scanner scanner, PrintStream out, PrintStream err) {
         this.maxValue = maxValue;
         this.minValue = 1;
         this.scanner = scanner;
         this.out = out;
         this.err = err;
-        this.solution = this.generateRandomNumber(this.minValue, this.maxValue);
+        this.solution = NumberGuessingGame.generateRandomNumber(this.minValue, this.maxValue);
     }
 
-    protected int generateRandomNumber(int min, int max) {
-        Random prng = new Random();
+    /**
+     * Generates a pseudo-random number between the two provided values.
+     * 
+     * @param min
+     *            the lowest allowable number
+     * @param max
+     *            the highest allowable number
+     * @return a pseudo-random number between the two provided values
+     * 
+     * @exception IllegalArgumentException
+     *                when {@code min} is less than {@code max}
+     * @see java.util.Random#nextInt
+     */
+    public static int generateRandomNumber(int min, int max) {
+        if (max == min) {
+            return max;
+        } else if (max < min) {
+            throw new IllegalArgumentException("Max must not be smaller than min.");
+        }
+        if (NumberGuessingGame.prng == null) {
+            NumberGuessingGame.prng = new Random();
+        }
+        // Random.nextInt generates a number between 0 and n, where n is the
+        // provided upper bound, inclusive of 0 but exclusive of n. 1 must be
+        // added to max to allow the upper bound to be included. Next, min is
+        // subtracted to make an equivalent range beginning at 0. Once a
+        // number is selected from that range, it is added back to min before
+        // being returned.
         int upperBound = (max + 1) - min;
-        int result = prng.nextInt(upperBound);
+        int result = NumberGuessingGame.prng.nextInt(upperBound);
         return min + result;
     }
 
+    /**
+     * Gets an integer guess from the user using the provided Scanner and
+     * PrintStreams.
+     * 
+     * @return a valid integer between the minimum and maximum values, as
+     *         supplied by the user.
+     */
     protected int getGuess() {
+        // In this case there is only one possible guess.
         if (this.minValue == this.maxValue) {
             this.out.printf("The only option left is %d!%n", this.minValue);
             return this.minValue;
         }
         int result = 0;
+        // Loop until the user provides an integer in the appropriate range
         do {
             this.out.printf("Enter a number between %d and %d:  ", this.minValue, this.maxValue);
+            // Get a string from the user input to simplify integer validation
             String scanned = this.scanner.next();
             try {
                 result = Integer.parseInt(scanned, this.scanner.radix());
@@ -55,6 +114,9 @@ public class NumberGuessingGame {
         return result;
     }
 
+    /**
+     * Runs the number guessing game.
+     */
     public void run() {
         int guess;
         int moves = 0;
@@ -75,6 +137,7 @@ public class NumberGuessingGame {
     }
 
     public static void main(String[] args) {
+        // Create an instance of the class and run the game.
         NumberGuessingGame self = new NumberGuessingGame();
         self.run();
     }
