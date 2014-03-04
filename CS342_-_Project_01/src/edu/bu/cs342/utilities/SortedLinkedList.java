27e7d3a7e35bd29f2ad0d9ff5c5b5120d8e53db1
Michael Donnelly <MichaelVDonnelly@gmail.com>
2014-03-04 07:16:41 -0500

    javadoc for SortedLinkedList

--- CS342_-_Project_01/src/edu/bu/cs342/utilities/SortedLinkedList.java
+++ CS342_-_Project_01/src/edu/bu/cs342/utilities/SortedLinkedList.java
@@ -3,8 +3,27 @@ package edu.bu.cs342.utilities;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * A double linked list which stores elements in ascending order.
+ * 
+ * @author Michael Donnelly
+ * 
+ * @param <E>
+ *            Type Type of list elements, implementing {@codeComparable<E>}
+ * @see java.lang.Comparable
+ */
 public class SortedLinkedList<E extends Comparable<E>> implements Iterable<E> {
 
+    /**
+     * A double linked list node.
+     * 
+     * The node is mutable, while the enclosed payload is not.
+     * 
+     * @author Michael Donnelly
+     * 
+     * @param <T>
+     *            Type Type Node payload, implementing {@codeComparable<E>}
+     */
     public class SortedLinkedListNode<T extends Comparable<T>> implements Comparable<T> {
         private final T payload;
         private SortedLinkedListNode<T> nextNode;
@@ -21,40 +40,88 @@ public class SortedLinkedList<E extends Comparable<E>> implements Iterable<E> {
             this.setPrevious(previous);
         }
 
+        /**
+         * Get the node following this node.
+         * 
+         * @return SortedLinkedListNode<T> The next node in the list, or null if
+         *         this is the last node.
+         */
         public SortedLinkedListNode<T> getNext() {
             return this.nextNode;
         }
 
+        /**
+         * Set the node following this node.
+         * 
+         * @param newNext
+         *            SortedLinkedListNode<T> The node following this node.
+         * @return SortedLinkedListNode<T> The previous value of
+         *         {@code getNext()}
+         */
         public SortedLinkedListNode<T> setNext(SortedLinkedListNode<T> newNext) {
             SortedLinkedListNode<T> result = this.getNext();
             this.nextNode = newNext;
             return result;
         }
 
+        /**
+         * Get the node previous to this node.
+         * 
+         * @return SortedLinkedListNode<T> The previous node in the list, or
+         *         null if this is the first node.
+         */
         public SortedLinkedListNode<T> getPrevious() {
             return this.previousNode;
         }
 
+        /**
+         * Set the node previous to this node.
+         * 
+         * @param newPrevious
+         *            SortedLinkedListNode<T> The node previous to this node.
+         * @return SortedLinkedListNode<T> The previous value of
+         *         {@code getPrevious()}
+         */
         public SortedLinkedListNode<T> setPrevious(SortedLinkedListNode<T> newPrevious) {
             SortedLinkedListNode<T> result = this.getPrevious();
             this.previousNode = newPrevious;
             return result;
         }
 
+        /**
+         * Get the payload enclosed by this node.
+         * 
+         * @return T The payload the node was created with.
+         */
         public T getValue() {
             return this.payload;
         }
 
+        /**
+         * Exposes {@code T.toString()}
+         * 
+         * @see Object#toString()
+         */
         @Override
         public String toString() {
             return this.getValue().toString();
         }
 
+        /**
+         * Exposes {@code T.compareTo(T)}
+         * 
+         * @see java.lang.Comparable#compareTo(Object)
+         */
         @Override
         public int compareTo(T o) {
             return this.payload.compareTo(o);
         }
 
+        /**
+         * Based upon {@code T.equals(Object)}
+         * 
+         * @see Object#equals(Object)
+         */
         @Override
         public boolean equals(Object o) {
             if (null == o) {
@@ -72,6 +139,11 @@ public class SortedLinkedList<E extends Comparable<E>> implements Iterable<E> {
             }
         }
 
+        /**
+         * Exposes {@code T.hashCode}
+         * 
+         * @see Object#hashCode()
+         */
         @Override
         public int hashCode() {
             return this.getValue().hashCode();
@@ -110,8 +182,8 @@ public class SortedLinkedList<E extends Comparable<E>> implements Iterable<E> {
 
     }
 
-    private SortedLinkedListNode<E> first;
-    private SortedLinkedListNode<E> last;
+    private SortedLinkedListNode<E> first; // head of the list
+    private SortedLinkedListNode<E> last; // tail of the list
     private int itemCount;
     private boolean allowDuplicates;
 
@@ -119,12 +191,27 @@ public class SortedLinkedList<E extends Comparable<E>> implements Iterable<E> {
         this(true);
     }
 
+    /**
+     * A linked list stored in ascending order.
+     * 
+     * @param allowDuplicates
+     *            boolean Whether the list stores multiple copies of equivalent
+     *            elements. (default: true)
+     */
     public SortedLinkedList(boolean allowDuplicates) {
         this.first = this.last = null;
         this.itemCount = 0;
         this.allowDuplicates = allowDuplicates;
     }
 
+    /**
+     * Finds the index of the element in the list.
+     * 
+     * @param element
+     *            E the payload to search the list for.
+     * @return int The 0-indexed count of the item, or -1 if the item is not
+     *         found.
+     */
     public int indexOf(E element) {
         int result = -1;
         int ct = 0;
@@ -138,77 +225,107 @@ public class SortedLinkedList<E extends Comparable<E>> implements Iterable<E> {
         return result;
     }
 
+    /**
+     * Indicates whether the element is a member of the list.
+     * 
+     * @param element
+     *            E the payload to search the list for.
+     * @return boolean true if the element appears at least once in the list,
+     *         otherwise false.
+     */
     public boolean contains(E element) {
         return (this.indexOf(element) != -1);
     }
 
+    /**
+     * Add an item to the list, in ascending order.
+     * 
+     * @param payload
+     *            E The item to be added.
+     * @return boolean whether the item was added. May return false if the
+     *         payload is null, or if the payload is a duplicate and duplicates
+     *         are not permitted.
+     */
     public boolean addItem(E payload) {
         SortedLinkedListNode<E> node = null, currentNode = null, previousNode = null;
         if (null == payload) {
-          return false;
+            return false;
         }
         currentNode = this.first;
         while (null != currentNode && currentNode.compareTo(payload) < 0) {
-          currentNode = currentNode.getNext();
+            currentNode = currentNode.getNext();
         }
         if (null == currentNode) {
-          previousNode = this.last;
+            previousNode = this.last;
         } else {
-          if (!this.allowDuplicates && currentNode.getValue().equals(payload)) {
-            return false;
-          }
-          previousNode = currentNode.getPrevious();
+            if (!this.allowDuplicates && currentNode.getValue().equals(payload)) {
+                return false;
+            }
+            previousNode = currentNode.getPrevious();
         }
         node = new SortedLinkedListNode<>(payload, currentNode, previousNode);
         if (null == node.getNext()) {
-          this.last = node;
+            this.last = node;
         } else {
-          node.getNext().setPrevious(node);
+            node.getNext().setPrevious(node);
         }
         if (null == node.getPrevious()) {
-          this.first = node;
+            this.first = node;
         } else {
-          node.getPrevious().setNext(node);
+            node.getPrevious().setNext(node);
         }
         ++this.itemCount;
         return true;
     }
 
+    /**
+     * The length of the linked list
+     * 
+     * @return int the length of the linked list.
+     */
     public int length() {
         return this.itemCount;
     }
 
+    // Remove the node, and splice the list back together.
     private void remove(SortedLinkedListNode<E> node) {
-      SortedLinkedListNode<E> prev = node.getPrevious(), next = node.getNext();
-      if (null == next) {
-        this.last = prev;
-      } else {
-        next.setPrevious(prev);
-      }
-      if (null == prev) {
-        this.first = next;
-      } else {
-        prev.setNext(next);
-      }
+        SortedLinkedListNode<E> prev = node.getPrevious(), next = node.getNext();
+        if (null == next) {
+            this.last = prev;
+        } else {
+            next.setPrevious(prev);
+        }
+        if (null == prev) {
+            this.first = next;
+        } else {
+            prev.setNext(next);
+        }
     }
 
+    /**
+     * Remove all instances of payload from the linked list.
+     * 
+     * @param payload
+     *            E the element to search for.
+     * @return true if elements were removed.
+     */
     public boolean removeItem(E payload) {
-      SortedLinkedListNode<E> currentNode = this.first;
-      boolean found = false;
-      while (null != currentNode) {
-        if (currentNode.getValue().equals(payload)) {
-          found = true;
-          this.remove(currentNode);
-        } else {
-          if (found) {
-            break;
-          }
+        SortedLinkedListNode<E> currentNode = this.first;
+        boolean found = false;
+        while (null != currentNode) {
+            if (currentNode.getValue().equals(payload)) {
+                found = true;
+                this.remove(currentNode);
+            } else {
+                if (found) {
+                    break;
+                }
+            }
+            currentNode = currentNode.getNext();
         }
-        currentNode = currentNode.getNext();
-      }
-      return found;
+        return found;
     }
-    
+
     @Override
     public boolean equals(Object o) {
         if (null == o) {
