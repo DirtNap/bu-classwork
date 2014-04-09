9fb5292d1cbc0efa4a55cd27aab15b60c27704e4
Michael Donnelly <MichaelVDonnelly@gmail.com>
2014-04-08 20:32:57 -0400

    Abstract class for CollectionHash

--- CS342_-_Project_03/src/edu/bu/cs342/p03/CollectionHash.java
+++ CS342_-_Project_03/src/edu/bu/cs342/p03/CollectionHash.java
@@ -1,5 +1,31 @@
 package edu.bu.cs342.p03;
 
-public interface CollectionHash {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+public abstract class CollectionHash<E> {
+    protected static class dummyOutputStream extends OutputStream {
+        @Override
+        public void write(int b) throws IOException {
+            ;
+        }
+    }
+
+    public abstract boolean add(E item);
+
+    public abstract E delete(E item);
+
+    public abstract void showHash(PrintStream output);
+
+    public abstract E traceSearch(E item, PrintStream output);
+
+    public boolean contains(E item) {
+        return (null != this.search(item));
+    }
+
+    public E search(E item) {
+        return this.traceSearch(item, new PrintStream(new dummyOutputStream()));
+    }
 
 }
