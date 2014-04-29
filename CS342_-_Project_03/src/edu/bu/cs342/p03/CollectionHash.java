100c076b6c9b26f05238cbe331e5ec082c7fd4c8
Michael Donnelly <MichaelVDonnelly@gmail.com>
2014-04-29 14:39:25 -0400

    Documentation added.

--- CS342_-_Project_03/src/edu/bu/cs342/p03/CollectionHash.java
+++ CS342_-_Project_03/src/edu/bu/cs342/p03/CollectionHash.java
@@ -4,6 +4,13 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
+/**
+ * A hash which is suitable for storing a collection of items.
+ * 
+ * @author Michael Donnelly
+ * 
+ * @param <E>
+ */
 public abstract class CollectionHash<E> {
     protected static class DummyOutputStream extends OutputStream {
         @Override
@@ -12,20 +19,57 @@ public abstract class CollectionHash<E> {
         }
     }
 
+    /**
+     * Attempt to add an item to the hash.
+     * 
+     * @return boolean whether there was space for the item in the hash table.
+     */
     public abstract boolean add(E item);
 
+    /**
+     * Delete all equivalent items from the hash.
+     * 
+     * @param item
+     *            E The item to delete.
+     * @return E The deleted item.
+     */
     public abstract E delete(E item);
 
+    /**
+     * Display the buckets of the hash, and their contents.
+     * 
+     * @param output
+     */
     public abstract void showHash(PrintStream output);
 
+    /**
+     * Search for an item, displaying the steps required to locate that item.
+     * 
+     * @param item
+     *            E The item to find.
+     * @param output
+     *            PrintStream The stream to write the trace to.
+     * @return int The index of the bucket containing the item, or -1 if it was
+     *         not found.
+     */
     public abstract int traceSearch(E item, PrintStream output);
-    
+
+    /**
+     * The number of elements in the array.
+     */
     public abstract int size();
 
+    /**
+     * Returns the result of {@code CollectionHash#search(Object)} on item > -1
+     */
     public boolean contains(E item) {
         return (-1 != this.search(item));
     }
 
+    /**
+     * Identical to {@link #traceSearch(Object, PrintStream)} with a dummy
+     * PrintStream
+     */
     public int search(E item) {
         return this.traceSearch(item, new PrintStream(new DummyOutputStream()));
     }
--- CS342_-_Project_03/src/edu/bu/cs342/p03/OpenAddressHash.java
+++ CS342_-_Project_03/src/edu/bu/cs342/p03/OpenAddressHash.java
@@ -3,12 +3,29 @@ package edu.bu.cs342.p03;
 import java.io.PrintStream;
 import java.util.Iterator;
 
+/**
+ * An Open-Address implementation of CollectionHash.
+ * 
+ * @author Michael Donnelly
+ * 
+ * @param <E>
+ *            Type Type for elements in the collection
+ */
 public class OpenAddressHash<E> extends CollectionHash<E> {
 
+    /**
+     * Distinguishes probing strategies
+     * 
+     */
     static enum ProbeType {
         QUADRATIC, LINEAR
     }
 
+    /**
+     * An iterator which provides quadratic probing for a set of buckets, then
+     * degrades to linear probing when quadratic probing is no longer effective.
+     * 
+     */
     class Probe implements Iterator<Integer>, Iterable<Integer> {
 
         private int startValue;
@@ -18,6 +35,14 @@ public class OpenAddressHash<E> extends CollectionHash<E> {
         private boolean[] visited;
         private ProbeType probeType;
 
+        /**
+         * Creates a probe of possible bucket indices for storing item.
+         * 
+         * @param item
+         *            Object The item to be stored.
+         * @param buckets
+         *            int The number of buckets in the hash.
+         */
         public Probe(Object item, int buckets) {
             this.bucketCount = buckets;
             this.startValue = item.hashCode() % buckets;
@@ -32,16 +57,27 @@ public class OpenAddressHash<E> extends CollectionHash<E> {
             return this;
         }
 
+        /**
+         * Updates the index via Quadratic Probing
+         */
         private void quadraticProbe() {
             this.lastIndex = ((this.startValue + (this.currentBase * this.currentBase++)) % this.bucketCount);
         }
 
+        /**
+         * Updates the index via Linear Probing
+         * 
+         * This probe skips indices already suggested by the Quadratic Probe.
+         */
         private void linearProbe() {
             for (; this.visited[this.lastIndex % this.bucketCount]; this.lastIndex = ++this.lastIndex
                     % this.bucketCount)
                 ;
         }
 
+        /**
+         * Returns true as long as all buckets have not been visited.
+         */
         @Override
         public boolean hasNext() {
             for (int i = 0; i < this.visited.length; ++i) {
@@ -52,6 +88,12 @@ public class OpenAddressHash<E> extends CollectionHash<E> {
             return false;
         }
 
+        /**
+         * Returns the next index.
+         * 
+         * At first, Quadratic Probing is used. Once the Quadratic algorithm
+         * becomes ineffective, the iterator degrades to linear probing.
+         */
         @Override
         public Integer next() {
             switch (this.probeType) {
@@ -78,6 +120,10 @@ public class OpenAddressHash<E> extends CollectionHash<E> {
 
     }
 
+    /**
+     * This CollectionHash is equal to any CollectionHash which has the same
+     * size and contains all of the same elements.
+     */
     @SuppressWarnings("unchecked")
     @Override
     public boolean equals(Object o) {
@@ -114,6 +160,12 @@ public class OpenAddressHash<E> extends CollectionHash<E> {
         this(31);
     }
 
+    /**
+     * An Open-Address implementation of CollectionHash.
+     * 
+     * @param size
+     *            int The number of buckets to allocate.
+     */
     public OpenAddressHash(int size) {
         this.initialSize = size;
         this.buckets = new Object[this.initialSize];
@@ -121,6 +173,14 @@ public class OpenAddressHash<E> extends CollectionHash<E> {
         this.count = 0;
     }
 
+    /**
+     * Get a #Probe for the specified item.
+     * 
+     * @param item
+     *            E the item which will be added to the hash
+     * @return Probe An Iterable Iterator for Integer which lists potentially
+     *         available buckets.
+     */
     protected Probe getProbe(E item) {
         return new Probe(item, this.initialSize);
     }
@@ -143,6 +203,9 @@ public class OpenAddressHash<E> extends CollectionHash<E> {
         return false;
     }
 
+    /**
+     * 
+     */
     @Override
     public E delete(E item) {
         E result = null;
