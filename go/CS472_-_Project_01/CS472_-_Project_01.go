3bbbf4484d4abb448b768f609860bbf9457bf71f
Michael Donnelly <MichaelVDonnelly@gmail.com>
2014-09-20 13:11:01 -0400

    Added GetBitSplit

--- go/CS472_-_Project_01/CS472_-_Project_01.go
+++ go/CS472_-_Project_01/CS472_-_Project_01.go
@@ -27,11 +27,20 @@ func getInstructions() (instructions []uint32) {
 }
 
 func main() {
-	//var err error
 	instructions := getInstructions()
-
 	for i := 0; i < len(instructions); i++ {
-		num, _ := disassembler.GetBitRangeValue(instructions[i], 4, 28)
-		fmt.Printf("0x%08X %032b %X %b\n", instructions[i], instructions[i], num, num)
+		fmt.Printf("0x%08X %032b", instructions[i], instructions[i])
+		if ranges, err := disassembler.GetBitSplit(instructions[i], 6, 5, 5, 5, 5, 6); err == nil {
+			var num uint32
+			var t string
+			if ranges[5] == 0 {
+				num = ranges[0]
+				t = "R"
+			} else {
+				num = ranges[5]
+				t = "I"
+			}
+			fmt.Printf(" %s %02X %02d\n", t, num, num)
+		}
 	}
 }
--- go/disassembler/bits.go
+++ go/disassembler/bits.go
@@ -1,6 +1,10 @@
 package disassembler
 
-import "errors"
+import (
+	"errors"
+//	"fmt"
+//	"os"
+)
 
 func GetBitMask(size int, shift uint8) (mask uint32, err error)  {
 	if 0 > size || int(shift) + size > 32 {
@@ -23,4 +27,18 @@ func GetBitRangeValue(value uint32, size int, shift uint8) (rangeValue uint32, e
 		rangeValue >>= shift
 	}
 	return
+}
+
+func GetBitSplit(value uint32, sizes...int) ([]uint32, error) {
+	var ranges []uint32 = make([]uint32, len(sizes), len(sizes))
+	var shift uint8
+	for  i := 0; i < len(sizes); i++ {
+		if rangeValue, err := GetBitRangeValue(value, sizes[i], shift); err == nil {
+			ranges[i] = rangeValue
+		} else {
+			return ranges[0:0], err
+		}
+		shift += uint8(sizes[i])
+	}
+	return ranges, nil
 }
