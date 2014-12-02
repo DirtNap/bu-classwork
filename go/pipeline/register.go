d02b4da9090a9ef9335449a2424261e61b1b2d37
Michael Donnelly <MichaelVDonnelly@gmail.com>
2014-12-02 16:50:58 -0500

    Restructure per project conventions

--- go/pipeline/register.go
+++ go/pipeline/register.go
@@ -2,16 +2,26 @@ package pipeline
 
 import "github.com/DirtNap/bu-classwork/go/disassembler"
 
-type Register struct {
-	Read registerSide
-	Write registerSide
-}
-type registerSide struct {
+type aluFunc func(int, int) int
+
+// var ALUOps = map[byte]aluFunc(){
+// 	0x000: func(x int, y int) {return x & y}
+// 	0x001: func(x int, y int) {return x | y}
+// 	0x002: func(x int, y int)
+// }
+
+type IFIDRegister struct {
 	Instruction uint32
-	DecodedInstruction disassembler.InstructionFields
-	Control uint32
 }
-func (reg *Register) Flip() {
-	reg.Read = reg.Write
-	reg.Write = registerSide{}
+
+type IDEXRegister struct {
+	DecodedInstruction disassembler.InstructionFields
+}
+
+type EXMEMRegister struct {
+	DecodedInstruction disassembler.InstructionFields
+}
+
+type MEMWBRegister struct {
+	DecodedInstruction disassembler.InstructionFields
 }
